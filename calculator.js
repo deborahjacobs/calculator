cd6f9c6b9129ab5081334c72b2137dce54904add
deborahjacobs <deborahjacobs44@gmail.com>
2022-06-24 16:44:15 +0100

    init

---
+++ calculator.js
@@ -0,0 +1,21 @@
+const num1 = parseFloat(prompt('Enter nr1:'));
+const num2 = parseFloat(prompt('Enter nr2:'));
+
+const operator = prompt('Enter operator(+,-,/,*');
+let result = 0;
+if(isNaN(num1) || isNaN(num2)){
+    alert('Wrong Input! Refresh the page and provide detail')
+}
+
+else{
+    if(operator== '+'){
+        result = num1 + num2;
+    }else if(operator == '-'){
+        result = num1 - num2;
+    }else if(operator == '/'){
+        result = num1 / num2;
+    }else if(operator == '*'){
+        result = num1 * num2;
+    }
+    alert(num1+operator+num2+'=' + result);
+}
